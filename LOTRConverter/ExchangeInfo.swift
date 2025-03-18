8b888c802087d74ee91c67a4fbe4b0cbcc510b75
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2025-03-18 16:20:06 -0500

    Diseñar la pantalla de información intercambio, ajustes para modo oscuro

--- LOTRConverter/ExchangeInfo.swift
+++ LOTRConverter/ExchangeInfo.swift
@@ -9,7 +9,87 @@ import SwiftUI
 
 struct ExchangeInfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack{
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3) // espaciado en tre letra
+                
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title3)
+                    .padding()
+                
+                HStack{
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 33)
+                }
+                
+                HStack{
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    Text("1 Gold Penny = 4 Silver Pieces")
+                    Image(.silverpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 33)
+                }
+                
+                HStack{
+                    Image(.silverpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    Text("1 Silver Piece = 4 Silver Pennies")
+                    Image(.silverpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 33)
+                }
+                
+                HStack{
+                    Image(.silverpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    Text("1 Silver Penny = 100 Cooper Pennies")
+                    Image(.copperpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 33)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Done")
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white) // para anular el color negro que se puso en el VStack para el modo oscuro
+
+            }
+            .foregroundStyle(.black) // Para el modo oscuro
+        }
     }
 }
 
