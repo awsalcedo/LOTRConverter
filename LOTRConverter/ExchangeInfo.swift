cd7e0d209c31e0c916cd47582355fa1086c144ac
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2025-03-18 16:19:37 -0500

    Diseñar la pantalla de información intercambio, ajustes para modo oscuro

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 18/3/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
