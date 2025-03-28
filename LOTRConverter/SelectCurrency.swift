87802e28a863a2b68d065e8568204f83ac7f603c
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2025-03-27 19:59:42 -0500

    Empezar a crear SelectCurrency

--- LOTRConverter/SelectCurrency.swift
+++ LOTRConverter/SelectCurrency.swift
@@ -1,5 +1,5 @@
 //
-//  ExchangeRate.swift
+//  SelectCurrency.swift
 //  LOTRConverter
 //
 //  Created by ALEX WLADIMIR SALCEDO SILVA on 19/3/25.
@@ -7,28 +7,16 @@
 
 import SwiftUI
 
-struct ExchangeRate: View {
+struct SelectCurrency: View {
     
-    let exchange: String
-    let leftImage: ImageResource
-    let rightImage: ImageResource
     
     var body: some View {
-        HStack{
-            Image(leftImage)
-                .resizable()
-                .scaledToFit()
-                .frame(height: 33)
+        ZStack {
             
-            Text(exchange)
-            Image(rightImage)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 33)
         }
     }
 }
 
 #Preview {
-    ExchangeRate(exchange: "1 Gold Piece = 4 Gold Pennies", leftImage: .goldpiece, rightImage: .goldpenny)
+    SelectCurrency()
 }
